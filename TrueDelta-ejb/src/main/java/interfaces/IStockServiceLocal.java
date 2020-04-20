ed5d50bfd9eacc88efa79caa4e0f978857860374
seif2 <seif2@192.168.1.11>
2020-04-20 23:33:20 +0100

    Stock Service 2.0

--- TrueDelta-ejb/src/main/java/interfaces/IStockServiceLocal.java
+++ TrueDelta-ejb/src/main/java/interfaces/IStockServiceLocal.java
@@ -1,9 +1,13 @@
 package interfaces;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Date;
 import java.util.List;
 
 import javax.ejb.Local;
 
+import entities.Company;
 import entities.Stock;
 
 @Local
@@ -12,8 +16,13 @@ public interface IStockServiceLocal {
 	public int addStock(Stock stock);
 	public List<Stock> findAllStocks();
 	public void deleteStock(int idStock);
-	public void updateStock(Stock stock);
+	public void updateStockByDate(Stock stock, Date newDate);
 	
 	public Stock findStockById(int idStock);
 	
+	public Stock getRealStockInformation(Company company) throws IOException;
+	
+	public int addCompany(Company company);
+	public Company getRealCompanyInformation(String symbol) throws UnsupportedEncodingException, IOException;
+	public void deleteCompany(int idCompany);
 }
--- TrueDelta-ejb/src/main/java/interfaces/IStockServiceRemote.java
+++ TrueDelta-ejb/src/main/java/interfaces/IStockServiceRemote.java
@@ -1,9 +1,15 @@
 package interfaces;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Date;
 import java.util.List;
 
 import javax.ejb.Remote;
 
+import org.json.JSONException;
+
+import entities.Company;
 import entities.Stock;
 
 @Remote
@@ -12,7 +18,13 @@ public interface IStockServiceRemote {
 	public int addStock(Stock stock);
 	public List<Stock> findAllStocks();
 	public void deleteStock(int idStock);
-	public void updateStock(Stock stock);
+	public void updateStockByDate(Stock stock, Date newDate);
 	
 	public Stock findStockById(int idStock);
+	
+	public Stock getRealStockInformation(Company company) throws IOException;
+	
+	public int addCompany(Company company);
+	public Company getRealCompanyInformation(String symbol) throws UnsupportedEncodingException, IOException, JSONException;
+	public void deleteCompany(int idCompany);
 }
