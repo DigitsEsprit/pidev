b8fb60ec8d430d4b5d44185d06f4b3b266f06665
seif2 <seif2@192.168.1.11>
2020-04-20 23:35:32 +0100

    Revert "Stock Service 2.0"
    This reverts commit ed5d50bfd9eacc88efa79caa4e0f978857860374.

--- TrueDelta-ejb/src/main/java/interfaces/IStockServiceRemote.java
+++ TrueDelta-ejb/src/main/java/interfaces/IStockServiceRemote.java
@@ -1,15 +1,9 @@
 package interfaces;
 
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.util.Date;
 import java.util.List;
 
 import javax.ejb.Remote;
 
-import org.json.JSONException;
-
-import entities.Company;
 import entities.Stock;
 
 @Remote
@@ -18,13 +12,7 @@ public interface IStockServiceRemote {
 	public int addStock(Stock stock);
 	public List<Stock> findAllStocks();
 	public void deleteStock(int idStock);
-	public void updateStockByDate(Stock stock, Date newDate);
+	public void updateStock(Stock stock);
 	
 	public Stock findStockById(int idStock);
-	
-	public Stock getRealStockInformation(Company company) throws IOException;
-	
-	public int addCompany(Company company);
-	public Company getRealCompanyInformation(String symbol) throws UnsupportedEncodingException, IOException, JSONException;
-	public void deleteCompany(int idCompany);
 }
